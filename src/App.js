bc49ce6f19db879c5fa4592d817d6a33c7671583
MarcosLopez99 <marcosleonor99@gmail.com>
2020-11-05 19:57:37 -0600

    Lasr commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useContext, Suspense } from 'react'
+import { GlobalStyle } from './styles/GlobalStyles'
+import { Logo } from './components/Logo'
+import { NavBar } from './components/NavBar'
+
+import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
+// import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import { NotFound } from './pages/NotFound'
+
+import { Router, Redirect } from '@reach/router'
+import { Context } from './Context'
+
+const Favs = React.lazy(() => import('./pages/Favs'))
+
+export const App = () => {
+  const { isAuth } = useContext(Context)
+
+  return (
+    <Suspense fallback={<div />}>
+      <GlobalStyle />
+      <Logo />
+      <Router>
+        <NotFound default />
+        <Home path='/' />
+        <Home path='/pet/:categoryId' />
+        <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect from='/favs' to='/login' />}
+        {!isAuth && <Redirect from='/user' to='/login' />}
+        {isAuth && <Redirect from='/login' to='/' />}
+        <Favs path='/favs' />
+        <User path='/user' />
+      </Router>
+
+      <NavBar />
+    </Suspense>
+  )
+}
